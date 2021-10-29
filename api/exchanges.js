96023e8b7ba4342b1e84debe9cba596f0534f05b
Thomas Tan <thomastanwz@gmail.com>
2021-10-28 23:31:16 -0500

    Now with a functioning state hook + fixed backend + functioning dropdown

--- api/exchanges.js
+++ api/exchanges.js
@@ -1,11 +1,24 @@
 //Node.JS serverless function as backend service
-
+//import cross-fetch since nodejs does not have built-in fetch
 const fetch = require("cross-fetch");
 
 module.exports = async (req, res) => {
-    const api_url = 'https://api.shrimpy.io/v1/binance/ticker'
-    const fetch_response = await fetch(api_url);
-    res.statusCode = 201;
+    //pass the requested parameter into our api url
+    const exchange = req.query.exchange;
+    const api_url = `https://api.shrimpy.io/v1/${exchange}/ticker`
+    // let fetch_response = await fetch(api_url);
+    let fetch_response;
+    //async fetch to see if our api call succeeded
+    await fetch(api_url)
+        .then(response => {
+            fetch_response = response;
+            res.statusCode = 201;
+        })
+        .catch(err => {
+            res.statusCode = 404;
+            console.log(err);
+        });
+
     const json = await fetch_response.json();
     res.json(json);
 }
--- src/App.js
+++ src/App.js
@@ -5,15 +5,23 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import './App_Background.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
+    //first, we declare state hooks to be used later
   const [btc, setBTC] = useState({name: "BTC", price: 0});
   const [eth, setETH] = useState({name: "ETH", price: 0});
+  const [options, setOPT] = useState([{exchange: 'ex1'}, {exchange: 'ex2'}]);
+
+  //hard coded data for testing
+    //TODO: Get from options
+  const exchange = "kraken";
 
   useEffect(() => {
     //makes an async fetch call to our serverless function and fetches data from source
     async function getData() {
-      const res = await fetch('/api/exchanges', {method: "GET"});
+      const res = await fetch(`/api/exchanges/?exchange=${exchange}`, {method: "GET"});
       let json = await res.json();
+      //We only want Ethereum and Bitcoin in our application
       json = json.filter(obj => {
         return obj.name === 'Ethereum' || obj.name === 'Bitcoin'
       });
@@ -26,13 +34,14 @@ function App() {
         name: json[1].name,
         price: json[1].priceUsd
       });
-      console.log(data);
+
     }
     getData();
   }, []);
 
   const data = [btc, eth];
 
+  //bar chart we use to visualize the data
   const barChart = (
       <BarChart width={850} height={500} data={data}>
         <CartesianGrid strokeDasharray="3 3" fill="#ffffff"/>
@@ -44,23 +53,46 @@ function App() {
       </BarChart>
   );
 
-  //move this to the right of chart
-  const dropDown = (
-      <DropdownButton id="dropdown-basic-button" title="Exchange Sources">
-          <Dropdown.Item href="#/action-1">Binance</Dropdown.Item>
-          <Dropdown.Item href="#/action-2">Bittrex</Dropdown.Item>
-          <Dropdown.Item href="#/action-3">CoinbasePro</Dropdown.Item>
-          <Dropdown.Item href="#/action-4">Kraken</Dropdown.Item>
-          <Dropdown.Item href="#/action-5">Kucoin</Dropdown.Item>
-          <Dropdown.Item href="#/action-3">Poloneix</Dropdown.Item>
-      </DropdownButton>
-  );
+
+  //reusable dropdown
+  function DropDown(props){
+      return (
+        <DropdownButton id="dropdown-basic-button" title={props.name} onSelect=
+            {props.id === '1'? dropDownHandler1: dropDownHandler2}>
+            <Dropdown.Item eventKey="binance">Binance</Dropdown.Item>
+            <Dropdown.Item eventKey="bittrex">Bittrex</Dropdown.Item>
+            <Dropdown.Item eventKey="coinbasepro">CoinbasePro</Dropdown.Item>
+            <Dropdown.Item eventKey="kraken">Kraken</Dropdown.Item>
+            <Dropdown.Item eventKey="kucoin">Kucoin</Dropdown.Item>
+            <Dropdown.Item eventKey="poloneix">Poloneix</Dropdown.Item>
+        </DropdownButton>
+    );
+  }
+
+  //dropdown button handler
+  const dropDownHandler1 = event => {
+      let copy = [...options];
+      copy[0].exchange = event;
+      setOPT(copy);
+      console.log(event);
+  }
+
+  const dropDownHandler2 = event => {
+      let copy = [...options];
+      copy[1].exchange = event;
+      setOPT(copy);
+      console.log(event);
+  }
 
   return (
     <main>
       <h1>BitCoin + Ethereum Exchange Reference</h1>
         <div>{barChart}</div>
-        {dropDown}
+        <div className="flexbox-container">
+            <div className="space"><DropDown name="exchange1" id='1'/></div>
+            <div><DropDown name="exchange2" id='2'/></div>
+            <div><h4>{options.map(home => <div>{home.exchange}</div>)}</h4></div>
+        </div>
     </main>
   );
 }
